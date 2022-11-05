b63d27f2fccc190411d4c00d170d11bba22195d0
Omti <81030511+Lequral@users.noreply.github.com>
2022-11-05 11:49:53 +0100

    Upload de ma version

---
+++ Galaxy Store/webPage/php/boutique/boutique.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Galaxy Store</title>
+    <link rel="stylesheet" href="./../../css/police.css">
+    <link rel="stylesheet" href="./../../css/header+footer.css">
+    <link rel="stylesheet" href="./../../css/boutique.css">
+</head>
+
+<body>
+    <header>
+        <h3>GALAXY STORE</h3>
+        <ul class="button">
+            <h5><a href="./../../index.php">A propos de nous</a></h5>
+            <h5 id="butSelected"><a href="./boutique.php">Boutique</a></h5>
+            <h5><a href="./../identificationClient/connexion.php">Connexion</a></h5>
+        </ul>
+    </header>
+
+    <main>
+    </main>
+
+    <footer></footer>
+
+</body>
+
+</html>
